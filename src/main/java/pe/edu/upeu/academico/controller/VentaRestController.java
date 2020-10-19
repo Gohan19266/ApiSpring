57627cf1659f370541bac79546d997cd4977d297
unknown <richeti1998@gmail.com>
2020-10-19 12:22:05 -0500

    ve

--- src/main/java/pe/edu/upeu/academico/controller/VentaRestController.java
+++ src/main/java/pe/edu/upeu/academico/controller/VentaRestController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import pe.edu.upeu.academico.entity.Producto;
 import pe.edu.upeu.academico.entity.Venta;
 import pe.edu.upeu.academico.services.VentaService;
 
